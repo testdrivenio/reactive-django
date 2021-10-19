0e995d9e44401a855c0898246c3d77ec24964ccd
Samuel Torimiro <efosonaname@gmail.com>
2021-10-19 15:29:24 +0100

    first commit

--- django_reactive_project/settings.py
+++ django_reactive_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
 
     # Third-party
     "django_unicorn",  # required for Unicorn to work
